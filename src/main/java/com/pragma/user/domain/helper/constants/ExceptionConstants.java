c87b18ddca172764d87b5604c0530b600f9e9554
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-13 14:22:09 -0500

    feat: add exception for invalid documentId and constant for exception message

--- src/main/java/com/pragma/user/domain/helper/constants/ExceptionConstants.java
+++ src/main/java/com/pragma/user/domain/helper/constants/ExceptionConstants.java
@@ -3,4 +3,5 @@ package com.pragma.user.domain.helper.constants;
 public class ExceptionConstants {
     public static final String CELL_PHONE_NUMBER_LENGTH_EXCEPTION_MESSAGE = "The cell phone number is no valid. Must have a maximum of 13 characters";
     public static final String CELL_PHONE_NUMBER_IS_NOT_A_NUMBER_MESSAGE = "The cell phone number is not a number";
+    public static final String DOCUMENT_ID_IS_NOT_A_NUMBER_MESSAGE = "The document id is not a number";
 }
