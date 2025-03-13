eacb2444e437e91f21cd5b32260458ca745485ba
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-13 15:23:47 -0500

    feat: add DTOs for user request and user response

---
+++ src/main/java/com/pragma/user/application/dto/response/UserResponseDto.java
@@ -0,0 +1,20 @@
+package com.pragma.user.application.dto.response;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String lastName;
+    private String documentId;
+    private String cellPhoneNumber;
+    private LocalDate dateOfBirth;
+    private String email;
+}
