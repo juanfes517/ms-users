f34ef5a99b3ae482effcda2a283fd06c345472b8
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-13 13:41:31 -0500

    test: add unit test for the domain User

---
+++ src/test/java/com/pragma/user/domain/model/UserTest.java
@@ -0,0 +1,62 @@
+package com.pragma.user.domain.model;
+
+import com.pragma.user.domain.exception.InvalidCellPhoneNumberException;
+import com.pragma.user.domain.helper.constants.ExceptionConstants;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void validateCellPhoneNumberWhenIsValidWithoutPlusSymbol() {
+        // Arrange
+        User user = new User();
+        user.setCellPhoneNumber("571232341543");
+
+        // Act
+        boolean result = user.validateCellPhoneNumber();
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void validateCellPhoneNumberWhenIsValidWithPlusSymbol() {
+        // Arrange
+        User user = new User();
+        user.setCellPhoneNumber("+571232341543");
+
+        // Act
+        boolean result = user.validateCellPhoneNumber();
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void validateCellPhoneNumberWhenItHas14Characters() {
+        // Arrange
+        User user = new User();
+        user.setCellPhoneNumber("+5712323415431");
+
+        // Act
+        InvalidCellPhoneNumberException exception = assertThrows(InvalidCellPhoneNumberException.class, user::validateCellPhoneNumber);
+
+        // Assert
+        assertEquals(ExceptionConstants.CELL_PHONE_NUMBER_LENGTH_EXCEPTION_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void validateCellPhoneNumberWhenItHasInvalidCharacters() {
+        // Arrange
+        User user = new User();
+        user.setCellPhoneNumber("+571232sdfasd");
+
+        // Act
+        InvalidCellPhoneNumberException exception = assertThrows(InvalidCellPhoneNumberException.class, user::validateCellPhoneNumber);
+
+        // Assert
+        assertEquals(ExceptionConstants.CELL_PHONE_NUMBER_IS_NOT_A_NUMBER_MESSAGE, exception.getMessage());
+    }
+}
