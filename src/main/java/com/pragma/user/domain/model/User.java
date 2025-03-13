4ab4455e4e44a2cb7ecf2c16df8f69d258404c13
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-13 14:39:00 -0500

    feat: add validations for the cellPhoneNumber and documentId fields

--- src/main/java/com/pragma/user/domain/model/User.java
+++ src/main/java/com/pragma/user/domain/model/User.java
@@ -1,5 +1,8 @@
 package com.pragma.user.domain.model;
 
+import com.pragma.user.domain.exception.InvalidCellPhoneNumberException;
+import com.pragma.user.domain.exception.InvalidDocumentIdException;
+import com.pragma.user.domain.helper.constants.ExceptionConstants;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -19,4 +22,30 @@ public class User {
     private String email;
     private String password;
     private Role role;
+
+    public boolean validateCellPhoneNumber() {
+        String cellPhoneNumberOfValidation = this.cellPhoneNumber;
+
+        if (cellPhoneNumberOfValidation.length() > 13) {
+            throw new InvalidCellPhoneNumberException(ExceptionConstants.CELL_PHONE_NUMBER_LENGTH_EXCEPTION_MESSAGE);
+        }
+
+        if (cellPhoneNumberOfValidation.charAt(0) == '+') {
+            cellPhoneNumberOfValidation = cellPhoneNumberOfValidation.substring(1);
+        }
+
+        if (!cellPhoneNumberOfValidation.chars().allMatch(Character::isDigit)) {
+            throw new InvalidCellPhoneNumberException(ExceptionConstants.CELL_PHONE_NUMBER_IS_NOT_A_NUMBER_MESSAGE);
+        }
+
+        return true;
+    }
+
+    public boolean validateDocumentId() {
+        if (!this.documentId.chars().allMatch(Character::isDigit)) {
+            throw new InvalidDocumentIdException(ExceptionConstants.DOCUMENT_ID_IS_NOT_A_NUMBER_MESSAGE);
+        }
+
+        return true;
+    }
 }
