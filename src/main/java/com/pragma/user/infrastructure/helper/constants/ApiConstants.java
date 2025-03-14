e89bcafb7e0ce9c9aadd3107a0fe8d5a1d7994b4
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-14 15:35:31 -0500

    docs: add swagger documentation

---
+++ src/main/java/com/pragma/user/infrastructure/helper/constants/ApiConstants.java
@@ -0,0 +1,9 @@
+package com.pragma.user.infrastructure.helper.constants;
+
+public class ApiConstants {
+    public static final String SAVE_OWNER_DESCRIPTION = "Save a new user owner";
+    public static final String OBJECT_CREATED_DESCRIPTION = "Object created";
+    public static final String  BAD_REQUEST_DESCRIPTION = "Object updated";
+    public static final String  NOT_FOUND_DESCRIPTION = "Object not found";
+    public static final String  CONFLICT_DESCRIPTION = "Conflict occurred";
+}
--- src/main/java/com/pragma/user/infrastructure/input/rest/UserRestController.java
+++ src/main/java/com/pragma/user/infrastructure/input/rest/UserRestController.java
@@ -3,6 +3,11 @@ package com.pragma.user.infrastructure.input.rest;
 import com.pragma.user.application.dto.request.OwnerRequestDto;
 import com.pragma.user.application.dto.response.UserResponseDto;
 import com.pragma.user.application.handler.IUserHandler;
+import com.pragma.user.infrastructure.helper.constants.ApiConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,8 +24,15 @@ public class UserRestController {
 
     private final IUserHandler userHandler;
 
+    @Operation(summary = ApiConstants.SAVE_OWNER_DESCRIPTION)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = ApiConstants.OBJECT_CREATED_DESCRIPTION, content = @Content),
+            @ApiResponse(responseCode = "400", description = ApiConstants.BAD_REQUEST_DESCRIPTION, content = @Content),
+            @ApiResponse(responseCode = "404", description = ApiConstants.NOT_FOUND_DESCRIPTION, content = @Content),
+            @ApiResponse(responseCode = "409", description = ApiConstants.CONFLICT_DESCRIPTION, content = @Content)
+    })
     @PostMapping
-    public ResponseEntity<UserResponseDto> saveUserOwner(@Valid @RequestBody OwnerRequestDto ownerRequestDto) {
+    public ResponseEntity<UserResponseDto> saveOwner(@Valid @RequestBody OwnerRequestDto ownerRequestDto) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(userHandler.saveOwner(ownerRequestDto));
