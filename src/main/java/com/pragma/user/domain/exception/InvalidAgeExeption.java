8f861a9408857caf0cd56d4887de125180df471b
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-13 20:57:35 -0500

    feat: add exception for invalid age

---
+++ src/main/java/com/pragma/user/domain/exception/InvalidAgeExeption.java
@@ -0,0 +1,8 @@
+package com.pragma.user.domain.exception;
+
+public class InvalidAgeExeption extends RuntimeException {
+    
+    public InvalidAgeExeption(String message) {
+        super(message);
+    }
+}
