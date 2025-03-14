dce3432196f5b25243b929309b9ce4c9126f6bda
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-13 21:18:53 -0500

    test: add unit test for the method that validates legal age

--- src/test/java/com/pragma/user/domain/model/UserTest.java
+++ src/test/java/com/pragma/user/domain/model/UserTest.java
@@ -1,9 +1,12 @@
 package com.pragma.user.domain.model;
 
+import com.pragma.user.domain.exception.InvalidAgeExeption;
 import com.pragma.user.domain.exception.InvalidCellPhoneNumberException;
 import com.pragma.user.domain.exception.InvalidDocumentIdException;
 import com.pragma.user.domain.helper.constants.ExceptionConstants;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,53 +14,41 @@ class UserTest {
 
     @Test
     void validateCellPhoneNumberWhenIsValidWithoutPlusSymbol() {
-        // Arrange
         User user = new User();
         user.setCellPhoneNumber("571232341543");
 
-        // Act
         boolean result = user.validateCellPhoneNumber();
 
-        // Assert
         assertTrue(result);
     }
 
     @Test
     void validateCellPhoneNumberWhenIsValidWithPlusSymbol() {
-        // Arrange
         User user = new User();
         user.setCellPhoneNumber("+571232341543");
 
-        // Act
         boolean result = user.validateCellPhoneNumber();
 
-        // Assert
         assertTrue(result);
     }
 
     @Test
     void validateCellPhoneNumberWhenItHas14Characters() {
-        // Arrange
         User user = new User();
         user.setCellPhoneNumber("+5712323415431");
 
-        // Act
         InvalidCellPhoneNumberException exception = assertThrows(InvalidCellPhoneNumberException.class, user::validateCellPhoneNumber);
 
-        // Assert
         assertEquals(ExceptionConstants.CELL_PHONE_NUMBER_LENGTH_EXCEPTION_MESSAGE, exception.getMessage());
     }
 
     @Test
     void validateCellPhoneNumberWhenItHasInvalidCharacters() {
-        // Arrange
         User user = new User();
         user.setCellPhoneNumber("+571232sdfasd");
 
-        // Act
         InvalidCellPhoneNumberException exception = assertThrows(InvalidCellPhoneNumberException.class, user::validateCellPhoneNumber);
 
-        // Assert
         assertEquals(ExceptionConstants.CELL_PHONE_NUMBER_IS_NOT_A_NUMBER_MESSAGE, exception.getMessage());
     }
 
@@ -76,10 +67,29 @@ class UserTest {
         User user = new User();
         user.setDocumentId("asfrdsasd");
 
-        // Act
         InvalidDocumentIdException exception = assertThrows(InvalidDocumentIdException.class, user::validateDocumentId);
 
-        // Assert
         assertEquals(ExceptionConstants.DOCUMENT_ID_IS_NOT_A_NUMBER_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void validateAgeWhenIsValid() {
+        User user = new User();
+        user.setDateOfBirth(LocalDate.of(2000, 5, 17));
+
+        boolean result = user.validateAge();
+
+        assertTrue(result);
+    }
+
+    @Test
+    void validateAgeWhenItHasIlegalAge() {
+        User user = new User();
+        int actualYear = LocalDate.now().getYear();
+        user.setDateOfBirth(LocalDate.of(actualYear - 10, 5, 17));
+
+        InvalidAgeExeption exeption = assertThrows(InvalidAgeExeption.class, user::validateAge);
+
+        assertEquals(ExceptionConstants.LEGAL_AGE_MESSAGE, exeption.getMessage());
     }
 }
