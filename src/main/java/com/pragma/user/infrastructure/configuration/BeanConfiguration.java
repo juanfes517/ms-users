d8ba839343ff1cdf8af0b68bb12f8b2f05e04fc1
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-14 11:10:53 -0500

    feat: add JPA user adapter and repository

---
+++ src/main/java/com/pragma/user/infrastructure/configuration/BeanConfiguration.java
@@ -0,0 +1,29 @@
+package com.pragma.user.infrastructure.configuration;
+
+import com.pragma.user.domain.api.IUserServicePort;
+import com.pragma.user.domain.spi.IUserPersistencePort;
+import com.pragma.user.domain.usecase.UserUseCase;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public IUserServicePort userServicePort(IUserPersistencePort userPersistencePort) {
+        return new UserUseCase(userPersistencePort);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
