37d694019a60a4026c8bf229b899260d720ea82a
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-17 14:16:54 -0500

    feat: implement the UserHasRole service in the user handler and controller

--- src/main/java/com/pragma/user/infrastructure/input/rest/UserRestController.java
+++ src/main/java/com/pragma/user/infrastructure/input/rest/UserRestController.java
@@ -12,10 +12,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -36,5 +33,10 @@ public class UserRestController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(userHandler.saveOwner(ownerRequestDto));
+    }
+
+    @GetMapping("/{user-id}/has-role")
+    public ResponseEntity<Boolean> userHasRole(@PathVariable("user-id") Long userId, @RequestParam String roleName) {
+        return ResponseEntity.ok(userHandler.userHasRole(userId, roleName));
     }
 }
