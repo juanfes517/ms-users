336d7f7cbe75711e78d54acaf27625ebf61e2401
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-19 15:30:36 -0500

    docs: add swagger documentation

--- src/main/java/com/pragma/user/infrastructure/helper/constants/ApiConstants.java
+++ src/main/java/com/pragma/user/infrastructure/helper/constants/ApiConstants.java
@@ -7,7 +7,11 @@ public class ApiConstants {
     public static final String SAVE_OWNER_DESCRIPTION = "Save a new user owner";
     public static final String USER_HAS_ROLE_DESCRIPTION = "Check if user has role";
     public static final String USER_HAS_EMAIL_DESCRIPTION = "Check if user has an email";
+    public static final String LOGIN_DESCRIPTION = "Login to the system";
+
+    public static final String OK_DESCRIPTION = "Request successful";
     public static final String SUCCESS_DESCRIPTION = "Request succeeded";
+    public static final String FORBIDDEN_DESCRIPTION = "Invalid credentials";
     public static final String OBJECT_CREATED_DESCRIPTION = "Object created";
     public static final String BAD_REQUEST_DESCRIPTION = "Bad request";
     public static final String NOT_FOUND_DESCRIPTION = "Object not found";
