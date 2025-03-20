d1bfbbad657ca5c23327f7000c2df4bc551dedf4
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-20 16:13:15 -0500

    feat: add endpoint to save a new employee

--- src/main/java/com/pragma/user/infrastructure/input/rest/UserRestController.java
+++ src/main/java/com/pragma/user/infrastructure/input/rest/UserRestController.java
@@ -1,6 +1,8 @@
 package com.pragma.user.infrastructure.input.rest;
 
+import com.pragma.user.application.dto.request.EmployeeRequestDto;
 import com.pragma.user.application.dto.request.OwnerRequestDto;
+import com.pragma.user.application.dto.response.EmployeeResponseDto;
 import com.pragma.user.application.dto.response.OwnerResponseDto;
 import com.pragma.user.application.handler.IUserHandler;
 import com.pragma.user.infrastructure.helper.constants.ApiConstants;
@@ -33,6 +35,20 @@ public class UserRestController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(userHandler.saveOwner(ownerRequestDto));
+    }
+
+    @Operation(summary = ApiConstants.SAVE_EMPLOYEE_DESCRIPTION)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = ApiConstants.OBJECT_CREATED_DESCRIPTION, content = @Content),
+            @ApiResponse(responseCode = "400", description = ApiConstants.BAD_REQUEST_DESCRIPTION, content = @Content),
+            @ApiResponse(responseCode = "404", description = ApiConstants.NOT_FOUND_DESCRIPTION, content = @Content),
+            @ApiResponse(responseCode = "409", description = ApiConstants.CONFLICT_DESCRIPTION, content = @Content)
+    })
+    @PostMapping("/employee")
+    public ResponseEntity<EmployeeResponseDto> saveEmployee(@Valid @RequestBody EmployeeRequestDto employeeRequestDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(userHandler.saveEmployee(employeeRequestDto));
     }
 
     @Operation(summary = ApiConstants.USER_HAS_ROLE_DESCRIPTION)
