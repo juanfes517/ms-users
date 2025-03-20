a6ea0cbdbc12c5d30eabbe02dfba67ef275507f2
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-20 17:02:50 -0500

    feat: add custom exception for restaurant not found

--- src/main/java/com/pragma/user/infrastructure/helper/constants/ExceptionConstants.java
+++ src/main/java/com/pragma/user/infrastructure/helper/constants/ExceptionConstants.java
@@ -5,6 +5,7 @@ public class ExceptionConstants {
     private ExceptionConstants() {}
 
     public static final String USER_NOT_FOUND = "User Not Found";
+    public static final String RESTAURANT_NOT_FOUND = "Restaurant Not Found";
     public static final String ROLE_NOT_FOUND = "Role Not Found";
     public static final String BAD_CREDENTIALS = "Invalid username or password";
 }
