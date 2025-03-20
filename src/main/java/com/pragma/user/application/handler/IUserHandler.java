c465d664a70747da448d7fd42ec869d2ed4a01b2
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-20 14:17:42 -0500

    feat: add saveEmployee method in the user hadler

--- src/main/java/com/pragma/user/application/handler/IUserHandler.java
+++ src/main/java/com/pragma/user/application/handler/IUserHandler.java
@@ -1,11 +1,15 @@
 package com.pragma.user.application.handler;
 
+import com.pragma.user.application.dto.request.EmployeeRequestDto;
 import com.pragma.user.application.dto.request.OwnerRequestDto;
+import com.pragma.user.application.dto.response.EmployeeResponseDto;
 import com.pragma.user.application.dto.response.OwnerResponseDto;
 
 public interface IUserHandler {
 
     OwnerResponseDto saveOwner(OwnerRequestDto ownerRequestDto);
+
+    EmployeeResponseDto saveEmployee(EmployeeRequestDto employeeRequestDto);
 
     boolean userHasRole(Long userId, String roleName);
 
