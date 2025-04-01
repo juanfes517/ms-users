1115cc556edfca6deaf6bb3ca5ecb6c3d0f4280e
juanfes517 <juan.escobar15@udea.edu.co>
2025-04-01 08:31:30 -0500

    refactor: delete unused imports

--- src/main/java/com/pragma/user/infrastructure/configuration/BeanConfiguration.java
+++ src/main/java/com/pragma/user/infrastructure/configuration/BeanConfiguration.java
@@ -1,16 +1,12 @@
 package com.pragma.user.infrastructure.configuration;
 
-import com.pragma.user.application.dto.request.OwnerRequestDto;
 import com.pragma.user.domain.api.IAuthServicePort;
 import com.pragma.user.domain.api.IRoleServicePort;
 import com.pragma.user.domain.api.IUserServicePort;
-import com.pragma.user.domain.model.User;
 import com.pragma.user.domain.spi.*;
 import com.pragma.user.domain.usecase.AuthUseCase;
 import com.pragma.user.domain.usecase.RoleUseCase;
 import com.pragma.user.domain.usecase.UserUseCase;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
